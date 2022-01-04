7ad7cf063d24082f5f4bc8788dd1152317d02c60
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-01-03 22:40:48 -0300

    slots, components dinamicos 3

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
 <template>
   <div id="app" class="container">
-    <h1>Forma padrão</h1>
-      <PostsLista :posts="posts"/>
-    <!-- distribuiçao ou projecao de conteudo SLOT-->
+    <h1>Componentes Dinâmicos</h1>
+    <button @click="componentSelecionado = 'Home'">Home</button>
+    <button @click="componentSelecionado = 'PostsLista'">Posts</button>
+    <button @click="componentSelecionado = 'Sobre'">Sobre</button>
+    
+    <!--:max="2" mantem 2 instâncias. Se tiver 3 destroi a que usou menos-->
+    <keep-alive :include="/Home|Sobre/">
+      <component 
+          :is="componentSelecionado"
+          v-bind="propsAtuais">
+      </component>
+    </keep-alive>
 
-    <hr>
-
-    <h2>Slots com escopo</h2>
-    <PostsLista :posts="posts"
-        v-slot="{ meuPost }">
-        <!-- desestruturação { meuPost } -->
-        <h2> {{ meuPost.titulo }}</h2>
-        <p> {{ meuPost.conteudo }}</p>
-        <small> {{ meuPost.autor }}</small>
-      <!-- <h2 slot="cabecalho">Components no Vue</h2> -->
-      
-      <!-- todo esse trecho abaixo que não está em algum slot específico será colocado dentro do slot default no post.vue -->
-      <!-- <p class='aplica-cor'></p>
-      <span>...</span> -->
-  
-      <!-- <small slot="rodape">Por {{ autor }}</small> -->
-
-      <!-- se eu nao tivesse o template abaixo iria colocar o texto padrao q eu decidi dentro do slot botão no component Post.vue -->
-      <!-- vou deixar comentado abaixo, para utilizar o valor default (ler mais)-->
-      <!-- <template slot="botao">Detalhes...</template> -->
-    </PostsLista>
   </div>
 </template>
 
 <script>
 import PostsLista from './components/PostsLista.vue'
+import Home from './components/Home.vue'
+import Sobre from './components/Sobre.vue'
 
 export default {
-  components: {
+  components:{
+    Home,
+    Sobre,
     PostsLista
   },
   data(){
     return{
-       posts:[{
-         id:1, titulo:"Components no Vue", conteudo:"Components são uma das peças mais importantes no Vue", autor: "Ponqueli"
-       },{
-         id:2, titulo:"Distribuindo conteúdo com slots", conteudo:"Slots podem ser usados como conteúdos de código HTML", autor: "Ponqueli"
-       }]
+      componentSelecionado:'Home',
+      posts:[{
+        id:1, titulo:"Components no Vue", conteudo:"Components são uma das peças mais importantes no Vue", autor: "Ponqueli"
+      },{
+        id:2, titulo:"Distribuindo conteúdo com slots", conteudo:"Slots podem ser usados como conteúdos de código HTML", autor: "Ponqueli"
+      }]
+    }
+  },
+  computed: {
+    propsAtuais(){
+      return this.componentSelecionado ==='PostsLista'
+        ? { posts: this.posts }
+        : {}
     }
   }
 }
